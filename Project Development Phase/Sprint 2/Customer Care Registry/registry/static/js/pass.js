0f0cf947858afc5b8cc390667fef1baaab2aba15
Arrush5 <110516655+Arrush5@users.noreply.github.com>
2022-11-18 19:12:26 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 2/Customer Care Registry/registry/static/js/pass.js
@@ -0,0 +1,9 @@
+function showPassword(){
+    var x = document.getElementById("password-input");
+
+    x.type = x.type == "password" ? "text" : "password";
+
+    var x = document.getElementById("password-input1");
+
+    x.type = x.type == "password" ? "text" : "password";
+}
