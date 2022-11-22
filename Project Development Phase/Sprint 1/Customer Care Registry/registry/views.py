b8eb83e29fd12d682d16226fb67f27ab9f5cd7b3
Arrush5 <110516655+Arrush5@users.noreply.github.com>
2022-11-22 17:54:36 +0530

    Update views.py
    db credentials removed

--- Project Development Phase/Sprint 1/Customer Care Registry/registry/views.py
+++ Project Development Phase/Sprint 1/Customer Care Registry/registry/views.py
@@ -13,7 +13,7 @@ pass_regex = r"^[A-Za-z0-9_-]*$"
 
 customer = Customer()
 
-conn = ibm_db.connect('DATABASE=bludb;HOSTNAME=2f3279a5-73d1-4859-88f0-a6c3e6b4b907.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=30756;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=tdn81266;PWD=7LY8okjAouJf3LoO', '', '')
+conn = ibm_db.connect('DATABASE=bludb;HOSTNAME=;PORT=;SECURITY=;SSLServerCertificate=;UID=;PWD=', '', '')
 
 @views.route('/logout')
 @login_required
@@ -46,7 +46,7 @@ def login():
 
         # Admin login
         if email == "admin.ccr@gmail.com":
-            if password == "admin.ccr@2022":
+            if password == "admin@ccr":
                 return redirect('/admin/tickets')
 
             else:
